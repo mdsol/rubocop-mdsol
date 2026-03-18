a2722778073197563f876eed60ba651cc8e59870
Yohei Kitamura <ykitamura@mdsol.com>
2026-03-18 10:48:55 +0900

    feat: Support ruby 3.2, 3.3, 3.4, 4.0, drop support for ruby < 3.2

--- spec/rubocop/cop/mdsol/log_with_data_spec.rb
+++ spec/rubocop/cop/mdsol/log_with_data_spec.rb
@@ -3,15 +3,15 @@ require "spec_helper"
 RSpec.describe RuboCop::Cop::Mdsol::LogWithData, :config do
   context "with the default config" do
     it "registers an offense if string interpolations exceed the default threshold" do
-      expect_offense(<<~RUBY)
-        Rails.logger.debug("with 3 string interpolations #\{foo\} #\{bar\} #\{baz\}")
+      expect_offense(<<~'RUBY')
+        Rails.logger.debug("with 3 string interpolations #{foo} #{bar} #{baz}")
         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `logger.debug_with_data(msg, data_hash)` instead. See https://github.com/mdsol/astinus#using-context-data-logging
       RUBY
     end
 
     it "does not register an offense if string interpolations does not exceed the default threshold" do
-      expect_no_offenses(<<~RUBY)
-        Rails.logger.debug("with 1 string interpolations #\{foo\}")
+      expect_no_offenses(<<~'RUBY')
+        Rails.logger.debug("with 1 string interpolations #{foo}")
       RUBY
     end
   end
@@ -20,23 +20,23 @@ RSpec.describe RuboCop::Cop::Mdsol::LogWithData, :config do
     let(:cop_config) { { "AllowedStringInterpolations" => 1 } }
 
     it "registers an offense if string interpolations exceed the configured threshold" do
-      expect_offense(<<~RUBY)
-        Rails.logger.debug("with 2 string interpolations #\{foo\} #\{bar\}")
+      expect_offense(<<~'RUBY')
+        Rails.logger.debug("with 2 string interpolations #{foo} #{bar}")
         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `logger.debug_with_data(msg, data_hash)` instead. See https://github.com/mdsol/astinus#using-context-data-logging
       RUBY
     end
 
     it "does not register an offense if string interpolations does not exceed the configured threshold" do
-      expect_no_offenses(<<~RUBY)
-        Rails.logger.debug("with 1 string interpolation #\{foo\}")
+      expect_no_offenses(<<~'RUBY')
+        Rails.logger.debug("with 1 string interpolation #{foo}")
       RUBY
     end
   end
 
   context "when forbidden Enumerable methods are used in string interpolation" do
     it "registers an offense (shortcut)" do
-      expect_offense(<<~RUBY)
-        Rails.logger.info("Deleted the following records: #\{records.map(&:id)\}")
+      expect_offense(<<~'RUBY')
+        Rails.logger.info("Deleted the following records: #{records.map(&:id)}")
         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `logger.info_with_data(msg, data_hash)` instead. See https://github.com/mdsol/astinus#using-context-data-logging
       RUBY
     end
@@ -44,8 +44,8 @@ RSpec.describe RuboCop::Cop::Mdsol::LogWithData, :config do
 
   context "when allowed Enumerable methods are used in string interpolation" do
     it "does not register an offense" do
-      expect_no_offenses(<<~RUBY)
-        Rails.logger.info("Deleted #\{records.size\} records")
+      expect_no_offenses(<<~'RUBY')
+        Rails.logger.info("Deleted #{records.size} records")
       RUBY
     end
   end
@@ -54,15 +54,15 @@ RSpec.describe RuboCop::Cop::Mdsol::LogWithData, :config do
     let(:cop_config) { { "LoggerOwners" => ["Utils"] } }
 
     it "checks against the configured logger" do
-      expect_offense(<<~RUBY)
-        Utils.logger.debug("with 3 string interpolations #\{foo\} #\{bar\} #\{baz\}")
+      expect_offense(<<~'RUBY')
+        Utils.logger.debug("with 3 string interpolations #{foo} #{bar} #{baz}")
         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `logger.debug_with_data(msg, data_hash)` instead. See https://github.com/mdsol/astinus#using-context-data-logging
       RUBY
     end
 
     it "checks against the default logger (Rails) as well" do
-      expect_offense(<<~RUBY)
-        Rails.logger.debug("with 3 string interpolations #\{foo\} #\{bar\} #\{baz\}")
+      expect_offense(<<~'RUBY')
+        Rails.logger.debug("with 3 string interpolations #{foo} #{bar} #{baz}")
         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use `logger.debug_with_data(msg, data_hash)` instead. See https://github.com/mdsol/astinus#using-context-data-logging
       RUBY
     end
@@ -70,8 +70,8 @@ RSpec.describe RuboCop::Cop::Mdsol::LogWithData, :config do
 
   context "when LoggerOwners is not configured" do
     it "does not check the offenses" do
-      expect_no_offenses(<<~RUBY)
-        Utils.logger.debug("with 3 string interpolations #\{foo\} #\{bar\} #\{baz\}")
+      expect_no_offenses(<<~'RUBY')
+        Utils.logger.debug("with 3 string interpolations #{foo} #{bar} #{baz}")
       RUBY
     end
   end
